48cfa3779d94955ff9e436f7d989f53351ac693b
kirillbunin <kirill@bunin.cz>
2020-06-15 17:19:22 +0200

    Changed nuxt plugin to be accordance with new structure of plugin itself

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,28 +1,10 @@
 import Vue from 'vue'
-import { Modal, Dialog, ModalsContainer, VueJsModal } from 'vue-js-modal'
+import VueJsModal from 'vue-js-modal/dist/ssr.nocss'
 
 export default function(ctx, inject) {
-  const options = Object.assign({}, <%= serialize(options) %>, { root: ctx.app })
+  const options =  <%= serialize(options) %>
   
-  const instance = new VueJsModal(Vue, options, {
-    componentName: options.componentName || 'Modal'
-  })
-  
-  /**
-   * Sets custom component name (if provided)
-   */
-  Vue.component(instance.context.componentName, Modal)
-  /**
-   * Registration of <Dialog/> component
-   */
-  if (options.dialog) {
-    Vue.component('VDialog', Dialog)
-  }
-  /**
-   * Registration of <ModalsContainer/> component
-   */
-  if (options.dynamic) {
-    Vue.component('ModalsContainer', ModalsContainer)
-  }
-  inject('modal', instance)
+  Vue.use(VueJsModal, options)
+
+  inject('modal', VueJsModal)
 }
